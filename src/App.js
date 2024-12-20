6386f6f061aa03f4abc4e95afecf442c3c7b6ea4
gmltmd021 <gmltmd021@naver.com>
2024-12-20 16:43:47 +0900

    test

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import styled, { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
+import { symbol } from 'prop-types';
 
 const Container = styled.View`
     flex: 1;
     background-color: ${({theme}) => theme.background};
     align-items: center;
     justify-content: center;
+`;
+
+const Title = styled.Text`
+    font-size: 40px;
+    font-weight: 600;
+    color: ${({theme}) => theme.main};
+    width: 100%;
+    align-items: flex-end;
+    padding: 0 20px;
 `;
 
 export default function App() {
