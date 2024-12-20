044b7ffa219c1eae61f1cbe52974f831e732fa61
gmltmd021 <gmltmd021@naver.com>
2024-12-20 16:40:06 +0900

    React Native Project Start

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+import styled, { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+
+const Container = styled.View`
+    flex: 1;
+    background-color: ${({theme}) => theme.background};
+    align-items: center;
+    justify-content: center;
+`;
+
+export default function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <Container>
+                <Text>new Project</Text>
+                <StatusBar style="auto" />
+            </Container>
+        </ThemeProvider>
+    );
+}
+
