0a2438fb114c0c345fd3fe85f9231783e01ffb51
gmltmd021 <gmltmd021@naver.com>
2024-12-30 14:37:50 +0900

    Input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'react-native';
 import styled, { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
+import Input from './components/input';
 
 const Container = styled.SafeAreaView`
     flex: 1;
-    background-color: ${({theme}) => theme.background};
+    background-color: ${({ theme }) => theme.background};
     align-items: center;
     justify-content: flex-start;
 `;
@@ -13,20 +14,34 @@ const Container = styled.SafeAreaView`
 const Title = styled.Text`
     font-size: 40px;
     font-weight: 600;
-    color: ${({theme}) => theme.main};
+    color: ${({ theme }) => theme.main};
     width: 100%;
     align-items: flex-end;
     padding: 0 20px;
 `;
 
 export default function App() {
+    const [newTask, setNewTask] = useState('');
+
+    const addTask = () => {
+        alert(newTask);
+        setNewTask('');
+    };
+    
+
     return (
         <ThemeProvider theme={theme}>
             <Container>
                 <Title>new Project</Title>
-                <StatusBar 
+                <StatusBar
                     barStyle="light-content"
                     backgroundColor={theme.background}
+                />
+                <Input
+                    placeholder="+ Add a Task"
+                    value={newTask}
+                    onChangeText={text => setNewTask(text)} 
+                    onSubmitEditing={addTask}
                 />
             </Container>
         </ThemeProvider>
