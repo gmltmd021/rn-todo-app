e375424f7dec19358df88c9077c62de2d55589c4
gmltmd021 <gmltmd021@naver.com>
2024-12-30 16:08:27 +0900

    icons, Task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
-import { StatusBar } from 'react-native';
+import { Dimensions, StatusBar } from 'react-native';
 import styled, { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 import Input from './components/input';
+import Task from './components/Task';
+// import IconButton from './components/IconButton';
+// import { icons } from './icons';
 
 const Container = styled.SafeAreaView`
     flex: 1;
@@ -20,14 +23,19 @@ const Title = styled.Text`
     padding: 0 20px;
 `;
 
+const List = styled.ScrollView`
+    flex: 1;
+    width: ${({width}) => width - 40}px;
+`;
+
 export default function App() {
+    const width = Dimensions.get('window').width;
     const [newTask, setNewTask] = useState('');
 
     const addTask = () => {
         alert(newTask);
         setNewTask('');
     };
-    
 
     return (
         <ThemeProvider theme={theme}>
@@ -40,11 +48,28 @@ export default function App() {
                 <Input
                     placeholder="+ Add a Task"
                     value={newTask}
-                    onChangeText={text => setNewTask(text)} 
+                    onChangeText={text => setNewTask(text)}
                     onSubmitEditing={addTask}
                 />
+                <List width={width}>
+                    <Task text="React Native" />
+                    <Task text="Expo" />
+                    <Task text="JavaScript" />
+                    <Task text="React Native" />
+                    <Task text="Expo" />
+                    <Task text="JavaScript" />
+                    <Task text="React Native" />
+                    <Task text="Expo" />
+                    <Task text="JavaScript" />
+                    <Task text="React Native" />
+                    <Task text="Expo" />
+                    <Task text="JavaScript" />
+                </List>
+                {/* <IconButton icon={icons.check} onPress={() => alert('check')} />
+                <IconButton icon={icons.uncheck} onPress={() => alert('uncheck')} />
+                <IconButton icon={icons.edit} onPress={() => alert('edit')} />
+                <IconButton icon={icons.delete} onPress={() => alert('delete')} /> */}
             </Container>
         </ThemeProvider>
     );
 }
-
---
+++ src/icons.js
@@ -0,0 +1,11 @@
+import Check from '../assets/icons/check.png'
+import Edit from '../assets/icons/edit.png';
+import Delete from '../assets/icons/delete.png';
+import Uncheck from '../assets/icons/uncheck.png';
+
+export const icons = {
+    check: Check,
+    edit: Edit,
+    delete: Delete,
+    uncheck: Uncheck,
+};
